9039e00a8b0b1db40e6249589704145e6840ec2a
ricardocruzlemus18 <ricardocruzlemus18@gmail.com>
2026-09-21 09:19:36 -0600

    feat: Creacion de panel en linea y seguridad BasicAuth

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
+from flask_basicauth import BasicAuth
 
+load_dotenv()
 app = create_app()
+
+app.config['BASIC_AUTH_USERNAME'] = os.getenv('BASIC_AUTH_USERNAME', 'empresa')
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv('BASIC_AUTH_PASSWORD', 'defaultpass')
+app.config['BASIC_AUTH_FORCE'] = True
+basic_auth = BasicAuth(app)
 
 if __name__ == '__main__':
     # debug=True recarga plantillas automáticamente
